f8e16dcc7afdd77f2186b3531e7f3ab024501921
Cheikh-Kanteye <cheikhkanteye.contact@gmail.com>
2023-08-23 20:10:00 +0000

    Add ContainerLayout to all Screen

--- src/components/ContainerLayout.tsx
+++ src/components/ContainerLayout.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SPACING } from "@misc/const";
+import { View } from "react-native";
 
-const ContainerLayout = ({ children }: { children: React.ReactNode }) => {
+interface Props {
+  children: React.ReactNode;
+  Left?: React.ReactNode
+  Middle?: React.ReactNode
+  Right?: React.ReactNode
+}
+
+const ContainerLayout = ({ children }: Props) => {
   return (
     <SafeAreaView
       style={{ flex: 1, paddingHorizontal: SPACING, backgroundColor: "white" }}
     >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      ></View>
       {children}
     </SafeAreaView>
   );
